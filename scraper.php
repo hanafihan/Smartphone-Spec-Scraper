6053b2bcdb3d98a9927394bc4a402cab7fd74042
Jonatan Van Hove <joon@joon.be>
2015-03-18 20:55:31 +0100

    # .

--- scraper.php
+++ scraper.php
@@ -84,8 +84,8 @@ class GSMAParser {
 	        $html_content_single = scraperwiki::scrape($m['link']);
 			$html_content_single_html = str_get_html($html_content_single);
 			
-			echo strtok($html_content_single_html,"\n") . "\n";
-			foreach ($html_content_single_html->find("div.specs-list tr") as $el_single) {
+			//echo strtok($html_content_single_html,"\n") . "\n";
+			foreach ($html_content_single_html->find("div#specs-list tr") as $el_single) {
 				echo '-- tr: ' . $el_single->find('a',0) . "\n";
 				if(stristr($el_single->find('a',0),'stat'))	{
 					$m['status'] = $el_single->find('td',1);
